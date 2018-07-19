b9980bf757eefa01e1ad84024fdd8df7a595e4ef
ensj <ensjtrans@gmail.com>
2018-07-19 19:18:23 +0900

    bug fix

--- commands/send.js
+++ commands/send.js
@@ -6,19 +6,20 @@ module.exports = {
 	args: true,
 	usage: '[user] [amount]',
 	execute(msg, args, block_io) {
-		if(!Number.isInteger(args[1])) return msg.reply('that\'s an invalid amount!');
+		var amount = Number(args[1]);
+		if(!Number.isInteger(amount)) return msg.reply('that\'s an invalid amount!');
 		
 		var taggedUser = msg.mentions.users.first();
 		block_io.withdraw_from_labels(
-			{'amounts': `${args[1]}`, 'from_labels': `${msg.author.id}`, 'to_labels': `${taggedUser.id}`}, 
+			{'amounts': `${amount}`, 'from_labels': `${msg.author.id}`, 'to_labels': `${taggedUser.id}`}, 
 			(error, data) => {
 				if(error) {
 					console.log(`\x1b[31mSend(${msg.author.username}): Error occurred: ${error.message}\x1b[0m`);
-					return msg.channel.send(`Failed to send \`${args[1]}\` coin(s) from ${msg.author.username}'s account to ${taggedUser.username}'s account.\nError: ${error.message}`);
+					return msg.channel.send(`Failed to send \`${amount}\` coin(s) from ${msg.author.username}'s account to ${taggedUser.username}'s account.\nError: ${error.message}`);
 				}
 
 				console.log(`${msg.author.username} withdrew ${data.data.amount_withdrawn} to ${taggedUser.username}.`);
-				msg.channel.send(`\`${args[1]}\` coin(s) successfully sent to ${taggedUser.username} with a transaction fee of 1 coin!`);
+				msg.channel.send(`\`${amount}\` coin(s) successfully sent to ${taggedUser.username} with a transaction fee of 1 coin!`);
 			}
 		);
 	},	
